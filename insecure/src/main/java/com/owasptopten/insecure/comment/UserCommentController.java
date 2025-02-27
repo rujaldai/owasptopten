f2a25c218cb1ec52a16c3b577ad7bcbc2347ab44
Rujal Manandhar <rujal@Rujals-MacBook-Pro.local>
2025-02-28 00:33:05 +0545

    Prepare insecure app

--- insecure/src/main/java/com/owasptopten/insecure/comment/UserCommentController.java
+++ insecure/src/main/java/com/owasptopten/insecure/comment/UserCommentController.java
@@ -6,10 +6,8 @@ import com.owasptopten.insecure.comment.service.UserCommentService;
 import com.owasptopten.insecure.userdetails.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +15,8 @@ import java.util.List;
 /**
  * SQL Injection, XSS & SSRF
  */
-@Controller
+
+@RestController
 @RequestMapping("/api/users/comments")
 @RequiredArgsConstructor
 public class UserCommentController {
